e131e22228e13aed5eecfc6003e84ee47d068ee3
relyonsky <42605690+relyonsky@users.noreply.github.com>
2019-09-02 17:47:17 +0800

    Add files via upload

---
+++ osd/osd.h
@@ -0,0 +1,20 @@
+/*
+ * osd.h
+ *
+ *  Created on: 2017Äê8ÔÂ8ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef SRC_OSD_H_
+#define SRC_OSD_H_
+
+#include "xbasic_types.h"
+#include "xparameters.h"
+#include "xstatus.h"
+
+void Camera_osd_init(void);
+void osd_draw_camera_text1(void);
+void osd_draw_camera_text2(void);
+
+
+#endif /* SRC_OSD_H_ */
